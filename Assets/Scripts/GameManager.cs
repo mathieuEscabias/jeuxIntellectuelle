1b328f1237d2a3798a3e23fa9a9778bb562edb1f
Théotime Eliet <theo.eliet@gmail.com>
2023-09-29 20:56:35 +0200

    build tower on click
    finished and clean

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,8 +22,6 @@ public class GameManager : MonoBehaviour
     {
         selectedTower = Instantiate(linkTowerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         playerLifeDisplay.GetComponent<UnityEngine.UI.Text>().text = playerLife.ToString();
-        // playerMoneyDisplay.GetComponent<UnityEngine.UI.Text>().text = playerMoney.ToString();
-        // playerScoreDisplay.GetComponent<UnityEngine.UI.Text>().text = playerScore.ToString();
     }
 
     // Update is called once per frame
@@ -38,7 +36,6 @@ public class GameManager : MonoBehaviour
         if( playerLife <= 0)
         {
             Debug.Log("Game Over");
-            // Quit game 
             Application.Quit();
         }
     }
@@ -47,8 +44,8 @@ public class GameManager : MonoBehaviour
         setPlayerLife(playerLife - damage);
     }
 
-    public void selectTower()
+    public void buildTower()
     {
-        Debug.Log("Select tower");
+      selectedTower = Instantiate(linkTowerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 }
