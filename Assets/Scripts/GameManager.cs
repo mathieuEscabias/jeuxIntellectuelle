293060d5ff70330aabea5ef6d0ad4f5bbb30c46d
Théotime Eliet <theo.eliet@gmail.com>
2023-09-29 19:15:13 +0200

    added gold and life counters

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int playerLife = 10;
+    public int playerMoney = 1000;
+    public int playerScore = 0;
+
+    // UI 
+    public GameObject playerLifeDisplay;
+    public GameObject playerMoneyDisplay;
+    public GameObject playerScoreDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLifeDisplay.GetComponent<UnityEngine.UI.Text>().text = playerLife.ToString();
+        // playerMoneyDisplay.GetComponent<UnityEngine.UI.Text>().text = playerMoney.ToString();
+        // playerScoreDisplay.GetComponent<UnityEngine.UI.Text>().text = playerScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setPlayerLife(int life)
+    {
+        playerLife = life;
+        playerLifeDisplay.GetComponent<UnityEngine.UI.Text>().text = playerLife.ToString();
+        if( playerLife <= 0)
+        {
+            Debug.Log("Game Over");
+            // close game 
+            Application.Quit();
+        }
+    }
+    public void playerLoseLife(int damage)
+    {
+        setPlayerLife(playerLife - damage);
+    }
+}
