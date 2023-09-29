8872e1d02532e25eec207ae927afb1fba1a47b69
mathieuEscabias <mathieuescabias@hotmail.com>
2023-09-29 18:16:46 +0200

    fonction vise

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -2,22 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tower : MonoBehaviour
-
+public class Tower : MonoBehaviour
 {
-    public 
+    public Transform target;
+    public float range = 1.5f;
+    public string enemyTag = "Enemy";
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+     void UpdateTarget()
+    {
+        GameObject[] ennemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in ennemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortDistance)
+            {
+                shortDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+        }
+        if (nearestEnemy != null && shortDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, range);
     }
 }
